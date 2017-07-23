cb3a63ebf3d7448ec73691f8cc196a1e947ee948
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-07-23 19:24:38 +0200

    refactored Select.java

--- src/selection_sort/Select.java
+++ src/selection_sort/Select.java
@@ -1,12 +1,15 @@
 package selection_sort;
 
-public class Select{
+public class Select {
 
-    public static void main(String[]args){
-        int[] arr1 = {5,3,8,2,9123,2525,11111111,23,0};
-        sortArray(arr1);
+    public static void main(String[] args) {
+        int[] arr1 = {5, 3, 8, 2, 9123, 2525, 11111111, 23, 0};
+        for (int number : sortArray(arr1)) {
+            System.out.println(number);
+        }
     }
-    public static void sortArray(int[] array1){
+
+    public static int[] sortArray(int[] array1) {
         int n = array1.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -18,9 +21,6 @@ public class Select{
 
             }
         }
-        for (int i : array1) {
-            System.out.println(i);
-        }
+        return array1;
     }
-
 }
